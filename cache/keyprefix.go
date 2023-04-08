7fa680eb770ed5e1dd2ec42a8927ce7bdd27c71f
fuzhifei <fu_zhifei@icloud.com>
2023-04-08 20:39:50 +0800

    feature(cache): cache key

--- cache/keyprefix.go
+++ cache/keyprefix.go
@@ -2,25 +2,31 @@ package cache
 
 import (
 	"fmt"
+	"github.com/fzf-labs/fpkg/conv"
 	"strings"
 	"time"
 )
 
 type KeyPrefixes struct {
-	List map[string]KeyPrefix
+	ServerName string
+	List       map[string]KeyPrefix
 }
 
 // NewKeyPrefixes 实例化缓存key前缀集合
-func NewKeyPrefixes() *KeyPrefixes {
-	return &KeyPrefixes{List: make(map[string]KeyPrefix)}
+func NewKeyPrefixes(serverName string) *KeyPrefixes {
+	return &KeyPrefixes{
+		ServerName: serverName,
+		List:       make(map[string]KeyPrefix),
+	}
 }
 
-// NewCacheKeyPrefix 添加一个缓存prefix
-func (p *KeyPrefixes) NewCacheKeyPrefix(prefix string, expirationTime time.Duration, remark string) *KeyPrefix {
+// AddKeyPrefix 添加一个缓存prefix
+func (p *KeyPrefixes) AddKeyPrefix(prefix string, expirationTime time.Duration, remark string) *KeyPrefix {
 	if _, ok := p.List[prefix]; ok {
 		panic(fmt.Sprintf("cache key %s is exsit, please change one", prefix))
 	}
 	key := KeyPrefix{
+		ServerName:     p.ServerName,
 		PrefixName:     prefix,
 		Remark:         remark,
 		ExpirationTime: expirationTime,
@@ -29,8 +35,21 @@ func (p *KeyPrefixes) NewCacheKeyPrefix(prefix string, expirationTime time.Durat
 	return &key
 }
 
+// Document 导出MD文档
+func (p *KeyPrefixes) Document() string {
+	str := `|ServerName|PrefixName|TTL(s)|Remark` + "\n" + `|--|--|--|--|` + "\n"
+
+	if len(p.List) > 0 {
+		for _, m := range p.List {
+			str += `|` + p.ServerName + `|` + m.PrefixName + `|` + conv.String(m.ExpirationTime.Seconds()) + `|` + m.Remark + `|` + "\n"
+		}
+	}
+	return str
+}
+
 // KeyPrefix 缓存key前缀
 type KeyPrefix struct {
+	ServerName     string
 	PrefixName     string
 	Remark         string
 	ExpirationTime time.Duration
@@ -42,9 +61,9 @@ func (p *KeyPrefix) BuildCacheKey(keys ...string) *Key {
 		keyPrefix: p,
 	}
 	if len(keys) == 0 {
-		cacheKey.buildKey = p.PrefixName
+		cacheKey.buildKey = strings.Join(append([]string{p.ServerName, p.PrefixName}), ":")
 	} else {
-		cacheKey.buildKey = strings.Join(append([]string{p.PrefixName}, keys...), ":")
+		cacheKey.buildKey = strings.Join(append([]string{p.ServerName, p.PrefixName}, keys...), ":")
 	}
 	return &cacheKey
 }
